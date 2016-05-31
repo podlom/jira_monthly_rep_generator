d8d7ee53bdaee5cb6ec2e248d986b5fef3be74ea
Shkodenko V. Taras <podlom@users.noreply.github.com>
2016-05-31 18:41:33 +0300

    Update get_current_year_month_total_work_hours.py

--- get_current_year_month_total_work_hours.py
+++ get_current_year_month_total_work_hours.py
@@ -13,9 +13,10 @@ today_year = today.strftime('%Y')
 conn = connect(r'/home/taras/my_scripts/wot.db')
 curs = conn.cursor()
 
-curs.execute("select task_id, sum(hours) from worklog where date like '%s-%s%%' group by task_id" % (today_year, today_month))
-# print curs.fetchall()
+curs.execute("select w.task_id, sum(w.hours), t.title from worklog w left join task_title t on t.task_id = w.task_id where w.date like '%s-%s%%' group by w.task_id" % (today_year, today_month))
 for row in curs:
-	# print(row)
-	(task_id, worked_hours) = row
-	print('%s\t%d' % (task_id, worked_hours))	
+	(task_id, worked_hours, task_title) = row
+	disp_title = ''
+	if task_title is not None:
+		disp_title = task_title
+	print('%s %s\t%d' % (task_id, disp_title, worked_hours))
